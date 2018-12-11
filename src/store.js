46231fa9ef8ee282e4efcfc8597aa74bab61393a
Selvin Norman <selvin715@gmail.com>
2018-12-11 13:11:03 -0800

    css changes and updated how store is constructed  due to package updates.

--- src/store.js
+++ src/store.js
@@ -1,22 +1,26 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { loadAuthToken } from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
 import { setAuthToken, refreshAuthToken } from './actions/auth';
-import { subscriptionReducer } from './reducers'
+import { subscriptionReducer } from './reducers';
 
-const store = createStore(
-    combineReducers({
-        form: formReducer,
-        subscription: subscriptionReducer,
-        auth: authReducer,
-        protectedData: protectedDataReducer 
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+const rootReducer = combineReducers({
+  form: formReducer,
+  subscription: subscriptionReducer,
+  auth: authReducer,
+  protectedData: protectedDataReducer 
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(rootReducer,
+  composeEnhancer(applyMiddleware(thunk)),
 );
+
 // Hydrate the authToken from localStorage if it exist
 const authToken = loadAuthToken();
 if (authToken) {
