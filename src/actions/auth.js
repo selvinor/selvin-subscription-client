f6ad1b8f34273ad8ab2f897ba9be57d8c209a70e
Selvin Norman <selvin715@gmail.com>
2018-12-20 10:18:39 -0800

    fixing login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -38,7 +38,7 @@ export const authError = error => ({
     error
 });
 
-// Stores the auth token in state and localStorage, and decodes and stores
+// Stores the auth token in state and localStorage, decodes and stores
 // the user data stored in the token
 const storeAuthInfo = (authToken, dispatch) => {
     const decodedToken = jwtDecode(authToken);
